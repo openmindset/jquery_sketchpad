a0468d99d290928ab7d8868b52e2b2bcc56c4de2
openmindset <fdavis712@gmail.com>
2015-08-06 06:44:48 -0400

    sketching action is working

--- js/sketchpad.js
+++ js/sketchpad.js
@@ -1,16 +1,21 @@
-var num_grids = 16;
+var num_grids_hw = 16;
+var total_num_grids = num_grids_hw * num_grids_hw;
 var container_width = 640;
 var div = document.createElement('div');
 
 function drawGrids(num_grid){
-	var grid_size = container_width / num_grids;
+	var grid_size = container_width / num_grids_hw;
 
-	for (i = 0; i < num_grids; i++) {
+	for (i = 0; i < total_num_grids; i++) {
 		$("#container").append("<div class='grid_square'></div>");
 	};
 
 };
 
 $(document).ready(function(){
-	drawGrids(num_grids)	
+	drawGrids(total_num_grids);
+
+	$(".grid_square").mouseenter(function() {
+		$(this).addClass('shaded');
+	});
 });

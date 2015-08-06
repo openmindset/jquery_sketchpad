339066bc0350147b71d2ed91b0e084632c76fc9c
openmindset <fdavis712@gmail.com>
2015-08-06 07:15:01 -0400

    reset function added, does not update css yet

--- js/sketchpad.js
+++ js/sketchpad.js
@@ -3,14 +3,27 @@ var total_num_grids = num_grids_hw * num_grids_hw;
 var container_width = 640;
 var div = document.createElement('div');
 
-function drawGrids(num_grid){
+function drawGrids(num_of_grids){
 	var grid_size = container_width / num_grids_hw;
+	$('.grid_square').height(grid_size);
+	$('.grid_square').width(grid_size);
+		
 
-	for (i = 0; i < total_num_grids; i++) {
+	for (i = 0; i < num_of_grids; i++) {
 		$("#container").append("<div class='grid_square'></div>");
+		
 	};
 
 };
+
+function resetGrids(){
+	$("#container").empty();
+	num_grids_hw = prompt("Enter resolution (number of grid squares):");
+	total_num_grids = num_grids_hw * num_grids_hw;
+	drawGrids(total_num_grids);
+};
+
+
 
 $(document).ready(function(){
 	drawGrids(total_num_grids);
